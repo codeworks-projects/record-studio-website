ee08a4478268caa9e62bee2d0ffc4de9a600fdc9
Matteo Biasi <matteobiasi@flashbeing.com>
2026-02-28 16:36:40 +0100

    feat: improved marquee

--- components/dynamic/VimeoGallery.vue
+++ components/dynamic/VimeoGallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="gallery-ct">
-    <NuxtMarquee :speed="20" id="numbers" :pause-on-hover="true">
+    <NuxtMarquee :speed="20" id="numbers" :pause-on-hover="true" class="marquee-wrapper">
       <div v-for="(video, index) in videos" :key="index" class="vimeo-dynamic">
         <iframe
           class="vimeo-iframe"
@@ -11,6 +11,24 @@
         ></iframe>
       </div>
     </NuxtMarquee>
+
+    <!-- Mobile: simple horizontal snap-scroller (visible only on small screens) -->
+    <div role="list" class="snap-wrapper">
+      <div
+        v-for="(video, index) in videos"
+        :key="`snap-${index}`"
+        role="listitem"
+        class="vimeo-dynamic"
+      >
+        <iframe
+          class="vimeo-iframe"
+          :src="`https://player.vimeo.com/video/${video.vimeoId}?loop=1&autopause=0&dnt=1`"
+          frameborder="0"
+          allow="autoplay; fullscreen"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,6 +58,11 @@ const props = withDefaults(defineProps<Props>(), {})
     border: 0;
   }
 }
+
+.snap-wrapper {
+  @apply !hidden;
+}
+
 @media only screen and (min-width: 1420px) {
   .gallery-ct {
     @apply -ml-[calc((100vw-1420px)/2)];
@@ -52,6 +75,21 @@ const props = withDefaults(defineProps<Props>(), {})
 
   .vimeo-dynamic {
     @apply w-[250px] h-[440px] mx-4;
+  }
+
+  .marquee-wrapper {
+    @apply !hidden;
+  }
+
+  .snap-wrapper {
+    @apply !flex overflow-x-auto gap-2 px-5 py-4 snap-x snap-mandatory;
+    -webkit-overflow-scrolling: touch;
+
+    & .vimeo-dynamic {
+      @apply w-[250px] h-[440px] mx-0 flex-none;
+
+      scroll-snap-align: center;
+    }
   }
 }
 </style>
